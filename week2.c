2dda68ee7b4f458f00993292f61692061068da2b
praew-ww <praew.pa@ku,th>
2020-02-13 22:13:45 +0700

    @week2.c about printf gets

---
+++ week2.c
@@ -0,0 +1,42 @@
+// //เนื้อหาเรื่อง printf gets  สรุปแลป week2
+// #include <stdio.h>
+// #include <stdlib.h>
+// {
+//     //gets(name);รับได้แค่คีย์บอร์ด รับทั้งบรรทัดจนกว่าจะเจอ/n
+//     //fgets(name, 20, stdin);รับจากไหนก็ได้กำหนดเอาเอง
+//     char number[5];
+//     fgets(number), 5, stdin);
+//     int i =atoi(number);//ใช้กับ จำนวนเต็ม
+//     // enter (/n)ถือเป็น 1 คาแรคเตอร์
+//     char a = getchar();
+//     char b = getchar();
+//     // long l = atol(number);
+//     //double d = atof(number); ใช้กับ ดับเบิล หรือ จำนวนจริง
+//     puts(number); //รับแต่ str
+//     putchar(100);putchar('a');
+
+//                 //type boolean
+//             int fond = 0;
+//             float isExist = 0.0;
+//             char isOnline = '\0';
+
+//     if (i>=0 && i <= 10){//ใช้กับการตัดสินใจะไรซักอย่าง ในลูป ใช้ปีกกา{} ในวงเล็บ ใส่เงื่อนไข
+//         printf("if = %d", 1>= 0 && 1 <= 10);// || is or
+//     }
+
+//                     loop
+// //while (count < i )
+// {
+//     //count++ * count - 5;//เก็บตัวแปร เค้าท์ขึ้นไปก่อน แล้วค่อยบอกช้ากว่า++count
+//     //++count // ทำก่อนเค้าท์บวกบวก
+//     printf("%d\n"), count );
+
+// }
+// for (int j = 0; j<i; j++){// for loop ช่วงสอง เป็นเงื่อนไขการทำลูป ถ้าเป็นจริงขะทำลูปต่อ
+// //ถ้าอยากให้เป็นอินฟินิตลูป ให้ใส่ while(1){คือวายทรู
+
+// }
+// }
+//             }
+// }
+// // %02d ใส่ 0 ข้างหน้า ให้ครบ 2 ตำแหน่ง
